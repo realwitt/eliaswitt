7b7b82f99798b5879aa1eeed37c3089fe7bb7758
Elias Witt <wittbrass@icloud.com>
2024-08-26 22:36:56 -0400

    add logo and navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			dark_blue: '#212738',
-			subtle_blue: '#5F70A0',
-			light_blue: '#9FA9C6',
-			accent_pink: '#F75590',
-			accent_purple: '#BDB2FF'
+		extend: {
+			colors: {
+				dark_blue: '#212738',
+				subtle_blue: '#5F70A0',
+				light_blue: '#9FA9C6',
+				accent_pink: '#F75590',
+				accent_purple: '#BDB2FF'
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 }
