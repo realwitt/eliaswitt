22476bbd456d38d24fd553536324a5af3dd7b249
Elias Witt <wittbrass@icloud.com>
2024-09-02 00:03:33 -0400

    datafaker: attempt to fix build take 3

--- src/components/fakermaker/FakerMaker3000.tsx
+++ src/components/fakermaker/FakerMaker3000.tsx
@@ -9,27 +9,29 @@ type FakerMaker3000Props = {
 
 export function FakerMaker3000({availableSchemaOptions}: FakerMaker3000Props) {
 
-    return (
-        <div className="w-full mt-5 mb-14 max-w-[1300px] mx-auto px-5 md:px-10">
+    if (availableSchemaOptions) {
+        return (
+            <div className="w-full mt-5 mb-14 max-w-[1300px] mx-auto px-5 md:px-10">
 
-            <Header title="save or restore session"/>
-            {/* todo */}
+                <Header title="save or restore session"/>
+                {/* todo */}
 
-            <Header title="fakers"/>
-            <Fakers availableFakers={availableSchemaOptions?.fakers}
-                    updatedFakerIndex={(i) => { console.log("faker index: " + i)
-                    }}/>
+                <Header title="fakers"/>
+                <Fakers availableFakers={availableSchemaOptions?.fakers}
+                        updatedFakerIndex={(i) => { console.log("faker index: " + i)
+                        }}/>
 
-            <Header title="makers"/>
-            <Makers availableMakers={availableSchemaOptions?.makers}
-                    updatedMakerIndex={(i) => console.log("maker index is: " + i)}/>
+                <Header title="makers"/>
+                <Makers availableMakers={availableSchemaOptions?.makers}
+                        updatedMakerIndex={(i) => console.log("maker index is: " + i)}/>
 
-            <Header title="active"/>
-            {/* <active /> */}
+                <Header title="active"/>
+                {/* <active /> */}
 
-            <Header title="data"/>
-            {/*todo*/}
+                <Header title="data"/>
+                {/*todo*/}
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
